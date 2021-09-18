01c43e7af801935e1d90948c89d50ce658160998
Nithin <nithinhs1996@gmail.com>
2021-09-18 17:18:41 +0530

    Static library usage with c++ in linux

---
+++ Static_library/lib/lib.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void func(void){
+
+std::cout<< "Hey im from the func\n";
+
+}
---
+++ Static_library/main/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "../lib/lib.hpp"
+
+int main(){
+std::cout << "I'm from main\n";
+func();
+}
